8e2009e0926a5566473da69d5bc75b47c9d4e44d
Kzrtt <99203878+Kzrtt@users.noreply.github.com>
2025-11-01 04:30:23 -0300

    [feat]: implementação da tela de login

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Livewire\LandingPage;
 use App\Livewire\PatientSignUp;
 use App\Livewire\DoctorSignUp;
 
+//? Áreas de Guest
 Route::get('/', LandingPage::class);
 Route::get('patient-sign-up', PatientSignUp::class);
 Route::get('doctor-sign-up', DoctorSignUp::class);
+
+//? Áreas que exigem autenticação
+Route::prefix('admin')->group(base_path('routes/admin.php'));

e94fb461e73a498a01892fb89d5c150742a29e17
Lucas Dopfer <lucas-dopfer@outlook.com>
2025-11-01 04:11:38 -0300

    Criação das migrations

---
+++ database/migrations/2025_11_01_064406_create_patient_reports_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patient_reports', function (Blueprint $table) {
+            $table->id('par_id');
+            $table->date('par_period_starts');
+            $table->date('par_period_end');
+            $table->string('par_status');
+            $table->string('par_medication');
+            $table->integer('par_score');
+            $table->text('par_cli_resume');
+            $table->foreignId('pat_id')->constrained('patients', 'pat_id')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patient_reports');
+    }
+};
